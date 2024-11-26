6048702fed1153350cdbb62affa1bc736123f084
v-s-97 <valerio.santini.97@gmail.com>
2024-11-26 17:18:46 +0100

    Manca rep fin e 5

--- Homework02/libs/models/poor_cnn.py
+++ Homework02/libs/models/poor_cnn.py
@@ -3,7 +3,8 @@ import torch
 
 class PoorPerformingCNN(nn.Module):
     def __init__(self):
-        super(PoorPerformingCNN, self).__init__()
+        #super(PoorPerformingCNN, self).__init__()
+        super().__init__()
         ##############################
         ###     CHANGE THIS CODE   ###
         ##############################  
@@ -11,14 +12,14 @@ class PoorPerformingCNN(nn.Module):
         self.relu1 = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
-        self.conv2 = nn.Conv2d(5, 8, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()
 
-        self.fc1 = nn.Linear(8 * 4 * 4, 28)
+        self.fc1 = nn.Linear(8 * 8 * 8, 10)
 
     def forward(self, x):
         x = self.pool(self.relu1(self.conv1(x)))
         x = self.pool(self.relu2(self.conv2(x)))
-        x = x.view(-1, 8 * 8 * 8)
+        x = x.view(x.size(0), -1)
         x = self.fc1(x)
         return x
